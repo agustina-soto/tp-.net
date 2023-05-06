389797661ff2343e138f50406e40adbc41ebd3a1
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-06 20:52:26 -0300

    Update IRepositorioPoliza.cs

--- Repositorios/IRepositorioPoliza.cs
+++ Repositorios/IRepositorioPoliza.cs
@@ -4,10 +4,6 @@ public interface IRepositorioPoliza {
 
   // Sus miembros van a ser publicos por defecto
   
-    void Ejecutar(IRepositorioPoliza p){
 
-    }
-
-    // No se que mas tiene que ir aca
 
 }
