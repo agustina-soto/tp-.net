2b4a4c8fddb201a5c1939ce4ccb4ce0893ed38f9
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-06 11:21:01 -0300

    Update Titular.cs

--- Aplicacion/Titular.cs
+++ Aplicacion/Titular.cs
@@ -3,23 +3,31 @@ namespace aplicacion;
 class Titular : Persona {
 
     private static int s_id = 0;
-    public string Direccion { get; set; }
-    public string Correo { get; set; }
+    public string? Direccion { get; set; }
+    public string? Correo { get; set; }
 
+    /*
     public Titular(string dni, string apellido, string nombre, string telefono, string direccion, string correo) : base(dni, apellido, nombre, telefono){
         Id = ++s_id;
         Direccion = direccion;
         Correo = correo;
     }
+    */
+    
+    public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){ /* Siempre se instancia con este en console --> deberiamos dejar el otro
+                                                                                                tambien? para mi conceptualmente es mejor, pero evidentemente no les
+                                                                                                importa mucho ese tema asiq no se que preferis hacer vos */
+        Id = ++s_id;
+    }
 
     public override string ToString() => base.ToString() + " " + this.Direccion + " " + base.Telefono + " " + this.Correo;
     
     /*
-    //puse los "?" para que no me tire warning
+    //puse los "?" para que no me tire warning --> que tiene que nos tire el warning? si le ponemos a estos el ? se lo tenemos que poner a todos
     private static int s_id = 0;
     public string? Direccion { get; set; }
     public string? Email { get; set; }
-    public List<Vehiculo>? Vehiculos { get; private set; } //esta bien el modificador de acceso???
+    public List<Vehiculo>? Vehiculos { get; private set; } //esta bien el modificador de acceso??? --> calculo que si, depende de como implementemos las interfaces q comunican un vehiculo y un titular
 
     public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){ //siempre se instancia con este en console
         Id = ++s_id;
