683d43808b5829862d189c534b30a0bf29999be1
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-06 13:24:05 -0300

    Update Persona.cs

--- Aplicacion/Persona.cs
+++ Aplicacion/Persona.cs
@@ -18,7 +18,7 @@ abstract class Persona {
     public override string ToString() => this.DNI + " " + this.Apellido + ", " + this.Nombre; // El telefono hay q imprimirlo manual en la clase derivada :)
 
     
-    /*public int Id { get; set; } //en console se accede a esta propiedad --> por qué el set es publico????????????
+    /*public int Id { get; protected set; } //en console se accede a esta propiedad --> por qué el set es publico???????????? xq me confundi yo :(
     public int DNI { get; set; } //en el trabajo lo declaran como entero
     public string Apellido { get; set; }
     public string Nombre { get; set; }
@@ -30,7 +30,14 @@ abstract class Persona {
         DNI = dni;
         Apellido = apellido;
         Nombre = nombre;
-    }*/
+    }
+    
+     protected Persona(int dni, string apellido, string nombre, string telefono): this (dni, apellido, nombre) //agrege el otro constructor 
+    {  
+        Telefono = telefono;
+    }
+    
+    */
     
     
 }
