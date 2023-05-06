283eba529dc5ea03d153cc06ca1e93aa3a069204
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-06 20:43:06 -0300

    Update Titular.cs

--- Aplicacion/Titular.cs
+++ Aplicacion/Titular.cs
@@ -4,8 +4,8 @@ class Titular : Persona {
 
     //chequear si agregar "?" a los tipo referencia, xq tira warning?? justificar en el tp explicativo en caso de ponerlos
     private static int s_id = 0;
-    public string Direccion { get; set; }
-    public string Email { get; set; }
+    public string? Direccion { get; set; }
+    public string? Email { get; set; }
     public List<Vehiculo>? Vehiculos { get; private set; } //esta bien el modificador de acceso??? --> calculo que si, depende de como implementemos las interfaces q comunican un vehiculo y un titular
 
     public Titular(int dni, string apellido, string nombre, string telefono, string direccion, string email) : base(dni, apellido, nombre, telefono){
@@ -16,7 +16,7 @@ class Titular : Persona {
     
     // Siempre se instancia usando este constructor en Console, pero dejamos el otro tambien porque conceptualmente nos parece que tiene que estar codificado
     // Despues vemos como lo justificamos bien en el documento sjsjs sacaremos la varita magica llamada <3 chamuyo <3
-    public Titular (int dni, string? apellido, string? nombre) : base(dni, apellido, nombre){
+    public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){
         Id = ++s_id;
     }
 
