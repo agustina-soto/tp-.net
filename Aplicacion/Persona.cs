c7f529430b0512a0b477014f271895e0a5267878
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-06 00:52:07 -0300

    Update Persona.cs

--- Aplicacion/Persona.cs
+++ Aplicacion/Persona.cs
@@ -17,4 +17,6 @@ abstract class Persona {
 
     public override string ToString() => this.DNI + " " + this.Apellido + ", " + this.Nombre; // El telefono hay q imprimirlo manual en la clase derivada :)
 //  ves  lo  que  escriboo???
+    
+    // chi
 }
