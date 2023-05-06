1c8f0ef57bd252c0790412efb43b534f9703bc67
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-06 11:37:55 -0300

    Update Vehiculo.cs

--- Aplicacion/Vehiculo.cs
+++ Aplicacion/Vehiculo.cs
@@ -6,7 +6,7 @@ class Vehiculo {
     public int Id { get; }
     public string Dominio { get; set; }
     public string Marca { get; set; }
-    public int FechaFabricacion { get; set; }
+    public DateTime FechaFabricacion { get; set; }
     public int TitularId { get; set; }
 
     public Vehiculo (string dominio, string marca, DateTime fechaFabricacion, int titularId){
