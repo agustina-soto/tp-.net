bc297af445834f419ba1d84a0cca801b41bf8e84
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-06 01:02:34 -0300

    Update Titular.cs

--- Aplicacion/Titular.cs
+++ Aplicacion/Titular.cs
@@ -13,4 +13,21 @@ class Titular : Persona {
     }
 
     public override string ToString() => base.ToString() + " " + this.Direccion + " " + base.Telefono + " " + this.Correo;
+    
+    /*
+    //puse los "?" para que no me tire warning
+    private static int s_id = 0;
+    public string? Direccion { get; set; }
+    public string? Email { get; set; }
+    public List<Vehiculo>? Vehiculos { get; private set; } //esta bien el modificar de acceso???
+
+    public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){ //siempre se instancia con este en console
+        Id = ++s_id;
+    }
+
+    //imprime de dos maneras :(
+    public override string ToString() => DNI + " " + Apellido + ", " + Nombre + " " + Direccion + " " + Telefono + " " + Email;
+
+    public override string ToString() => DNI + " " + Apellido + ", " + Nombre; //solucionar
+    */
 }
