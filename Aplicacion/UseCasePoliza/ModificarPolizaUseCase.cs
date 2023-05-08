0078959209d064c42b5855529e1da5a3b3a900e6
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:11:19 -0300

    Create ModificarPolizaUseCase.cs

---
+++ Aplicacion/UseCasePoliza/ModificarPolizaUseCase.cs
@@ -0,0 +1,16 @@
+namespace Entrega.Aplicacion;
+
+class ModificarPolizaUseCase
+{
+    private readonly IRepositorioPoliza _repoPoliza;
+
+    public ModificarPolizaUseCase (IRepositorioPoliza repoP)
+    {
+        _repoPoliza = repoP;
+    }
+
+    public void Ejecutar (Poliza p)
+    {
+        _repoPoliza.modificarPoliza(p);
+    } 
+}
