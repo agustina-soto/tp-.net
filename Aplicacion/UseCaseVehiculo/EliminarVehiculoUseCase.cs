6af13822cc8726266dcfd3331c9e09b4ef3bff43
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:09:08 -0300

    Create EliminarVehiculoUseCase.cs

---
+++ Aplicacion/UseCaseVehiculo/EliminarVehiculoUseCase.cs
@@ -0,0 +1,16 @@
+namespace Entrega.Aplicacion;
+
+class EliminarVehiculoUseCase
+{
+    private readonly IRepositorioVehiculo _repoVehiculo;
+
+    public EliminarVehiculoUseCase (IRepositorioVehiculo repoV)
+    {
+        _repoVehiculo = repoV;
+    }
+
+    public void Ejecutar (int id)
+    {
+        _repoVehiculo.eliminarVehiculo(id);
+    } 
+}
