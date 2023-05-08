bd745c782491b97c6ed9e9ff187a39a687b09a11
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:13:45 -0300

    Create IRepositorioPoliza.cs

---
+++ Aplicacion/Interfaces/IRepositorioPoliza.cs
@@ -0,0 +1,9 @@
+namespace Entrega.Aplicacion;
+
+interface IRepositorioPoliza
+{
+    public void agregarPoliza(Poliza p); 
+    public void eliminarPoliza(int Id);
+    public void modificarPoliza(Poliza p);
+    public List<Poliza> listarPolizas();
+}
