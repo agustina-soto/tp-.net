023879d5eadf549abdf9747aedf8a70295bdf04b
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:05:50 -0300

    Create AgregarTitularUseCase.cs

---
+++ Aplicacion/UseCaseTitular/AgregarTitularUseCase.cs
@@ -0,0 +1,17 @@
+namespace Entrega.Aplicacion;
+
+class AgregarTitularUseCase
+{
+    private readonly IRepositorioTitular _repoTitular;
+    
+    public AgregarTitularUseCase (IRepositorioTitular repoT)
+    {
+        _repoTitular = repoT;
+    }
+
+    public void Ejecutar (Titular t)
+    {
+        _repoTitular.agregarTitular(t);
+        
+    } 
+}
