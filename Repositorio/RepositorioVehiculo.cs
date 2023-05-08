a610bff3f49b025344810833e3e5b3845e71c4a3
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:16:54 -0300

    Create RepositorioVehiculo.cs

---
+++ Repositorio/RepositorioVehiculo.cs
@@ -0,0 +1,24 @@
+namespace Entrega.Aplicacion; //va en entrega.repositorios
+
+
+class RepositorioVehiculo : IRepositorioVehiculo
+{
+    //implementar
+    public void agregarVehiculo(Vehiculo v)
+    {
+
+    }
+    public void eliminarVehiculo(int id)
+    {
+
+    }
+    public void modificarVehiculo(Vehiculo v)
+    {
+
+    }
+    public List<Vehiculo> listarVehiculos()
+    {
+        List<Vehiculo> lista = new List<Vehiculo>();
+        return lista;
+    }
+}
