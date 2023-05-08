5411e784f4f13697d72fd1d8d9de6d4c580b8d50
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:16:05 -0300

    Create RepositorioPoliza.cs

---
+++ Repositorio/RepositorioPoliza.cs
@@ -0,0 +1,24 @@
+namespace Entrega.Aplicacion; //va en entrega.repositorios
+
+
+class RepositorioPoliza : IRepositorioPoliza
+{
+    //implementar
+    public void agregarPoliza(Poliza p)
+    {
+
+    }
+    public void eliminarPoliza(int id)
+    {
+
+    }
+    public void modificarPoliza(Poliza p)
+    {
+
+    }
+    public List<Poliza> listarPolizas()
+    {
+        List<Poliza> lista = new List<Poliza>();
+        return lista;
+    }
+}
