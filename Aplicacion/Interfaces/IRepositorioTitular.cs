15a882cdbd0e02e881fc94f9b5fffa91d1d2ee99
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:14:06 -0300

    Create IRepositorioTitular.cs

---
+++ Aplicacion/Interfaces/IRepositorioTitular.cs
@@ -0,0 +1,10 @@
+namespace Entrega.Aplicacion;
+
+interface IRepositorioTitular
+{
+    public void agregarTitular(Titular t);
+    public void eliminarTitular(int id);
+    public void modificarTitular(Titular t);
+    public List<Titular> listarTitulares();
+    public void listarTitularesConSusVehiculos(); //chequear como implementarlo
+}
