1f12f2398dae1ca11d39884704292d036f73be1d
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:07:24 -0300

    Create ModificarTotularUseCase.cs

---
+++ Aplicacion/UseCaseTitular/ModificarTotularUseCase.cs
@@ -0,0 +1,16 @@
+namespace Entrega.Aplicacion;
+
+class ModificarTitularUseCase
+{
+    private readonly IRepositorioTitular _repoTitular;
+
+    public ModificarTitularUseCase (IRepositorioTitular repoT)
+    {
+        _repoTitular = repoT;
+    }
+
+    public void Ejecutar (Titular t)
+    {
+        _repoTitular.modificarTitular(t);
+    } 
+}
