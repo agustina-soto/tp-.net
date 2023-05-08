e8ddfea14fb5b2afdda2a1526004bfbe3fd3da14
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 20:57:43 -0300

    Update Persona.cs

--- Aplicacion/Persona.cs
+++ Aplicacion/Persona.cs
@@ -2,25 +2,28 @@ namespace aplicacion;
 
 abstract class Persona {
 
-    public int Id { get; protected set; }
-    public int DNI { get; set; }
-    public string? Apellido { get; set; }
-    public string? Nombre { get; set; }
+    public int Id { get; protected set; } 
+    public int DNI { get; set; } 
+    public string Apellido { get; set; }
+    public string Nombre { get; set; }
     public string? Telefono { get; set; }
 
-    protected Persona(int dni, string apellido, string nombre, string telefono){
-        DNI = dni;
-        Apellido = apellido;
-        Nombre = nombre;
-        Telefono = telefono;
-    }
-
-    protected Persona(int dni, string apellido, string nombre){
+    protected Persona (int dni, string apellido, string nombre)
+    {
         Id = -1;
         DNI = dni;
         Apellido = apellido;
         Nombre = nombre;
     }
 
-    public override string ToString() => this.DNI + " " + this.Apellido + ", " + this.Nombre;
+    protected Persona(int dni, string apellido, string nombre, string telefono): this (dni, apellido, nombre)
+    {  
+        Telefono = telefono;
+    }
+
+    public void actualizarId (int id) => Id = id;
+
+
+    public override string ToString() => this.DNI + " " + this.Apellido + ", " + this.Nombre; 
+
 }
