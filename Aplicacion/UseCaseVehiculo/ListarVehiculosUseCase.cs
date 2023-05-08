d21acdc2e3f1861a0bee1a3741a2b01909217d56
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 00:09:53 -0300

    Create ListarVehiculosUseCase.cs

---
+++ Aplicacion/UseCaseVehiculo/ListarVehiculosUseCase.cs
@@ -0,0 +1,16 @@
+namespace Entrega.Aplicacion;
+
+class ListarVehiculosUseCase
+{
+    private readonly IRepositorioVehiculo _repoVehiculo;
+
+    public ListarVehiculosUseCase (IRepositorioVehiculo repoV)
+    {
+        _repoVehiculo = repoV;
+    }
+
+    public List<Vehiculo> Ejecutar ()
+    {
+        return _repoVehiculo.listarVehiculos();
+    } 
+}
