179c1d5cbfa0619e4011942153d53f2614260167
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 21:00:55 -0300

    Update Titular.cs

--- Aplicacion/Titular.cs
+++ Aplicacion/Titular.cs
@@ -1,22 +1,19 @@
 namespace aplicacion;
 
-class Titular : Persona {
-
-    //chequear si agregar "?" a los tipo referencia, xq tira warning?? justificar en el tp explicativo en caso de ponerlos
+class Titular : Persona
+{
+    //puse los "?" para que no me tire warning
     public string? Direccion { get; set; }
     public string? Email { get; set; }
-    public List<Vehiculo>? Vehiculos { get; private set; } //esta bien el modificador de acceso??? --> calculo que si, depende de como implementemos las interfaces q comunican un vehiculo y un titular
+    public List<Vehiculo>? Vehiculos { get; private set; }
 
-    /*public Titular(int dni, string apellido, string nombre, string telefono, string direccion, string email) : base(dni, apellido, nombre, telefono){
-        Id = ++s_id;
-        Direccion = direccion;
-        Email = email;
-    }*/
-    
-    // Siempre se instancia usando este constructor en Console, pero dejamos el otro tambien porque conceptualmente nos parece que tiene que estar codificado
-    // Despues vemos como lo justificamos bien en el documento sjsjs sacaremos la varita magica llamada <3 chamuyo <3
-    public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){}
+    public Titular (int dni, string apellido, string nombre) : base(dni, apellido, nombre){} //siempre se instancia con este en console
 
     public override string ToString() => base.ToString() + " " + this.Direccion + " " + base.Telefono + " " + this.Email;
+
+    public Titular (int dni, string apellido, string nombre, string telefono, string direccion, string email) : base(dni, apellido, nombre, telefono){
+        Direccion = direccion;
+        Email = email;
+    }
 
 }
