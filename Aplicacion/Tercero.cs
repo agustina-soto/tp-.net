e2358173b62109842270e160b73864a8d9ade0e7
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 21:00:22 -0300

    Update Tercero.cs

--- Aplicacion/Tercero.cs
+++ Aplicacion/Tercero.cs
@@ -2,14 +2,14 @@ namespace aplicacion;
 
 class Tercero : Persona {
 
-    private static int s_Id = 0;
     public string? Aseguradora { get; set; }
-    public int SiniestroId { get; private set; } // estos accesos te parecen bien???
+    public int SiniestroId { get; private set; } // chequear los accesos
 
-    public Tercero(string dni, string apellido, string nombre, string telefono, string aseguradora, int idSiniestro) : base(dni, apellido, nombre, telefono){
-        Id = ++s_Id;
+    public Tercero(int dni, string apellido, string nombre, string telefono, string aseguradora, int idSiniestro) : base(dni, apellido, nombre, telefono){
         Aseguradora = aseguradora;
         SiniestroId = idSiniestro;
     }
+
+    public Tercero (int dni, string apellido, string nombre) : base(dni, apellido, nombre){}
 
 }
