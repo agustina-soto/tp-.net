8c26f658947c6d7447fd13000bb937e45a63108c
maitesechi <130006181+maitesechi@users.noreply.github.com>
2023-05-08 21:05:32 -0300

    Update AgregarPolizaUseCase.cs

--- Aplicacion/UseCasePoliza/AgregarPolizaUseCase.cs
+++ Aplicacion/UseCasePoliza/AgregarPolizaUseCase.cs
@@ -3,15 +3,31 @@ namespace Entrega.Aplicacion;
 class AgregarPolizaUseCase
 {
     private readonly IRepositorioPoliza _repoPoliza;
+    private readonly IRepositorioVehiculo _repoVehiculo;
 
-    public AgregarPolizaUseCase (IRepositorioPoliza repoP)
+    public AgregarPolizaUseCase (IRepositorioPoliza repoP, IRepositorioVehiculo repoV)
     {
         _repoPoliza = repoP;
+        _repoVehiculo = repoV;
     }
 
     public void Ejecutar (Poliza p)
     {
-        _repoPoliza.agregarPoliza(p);
-        
+        try
+        {
+            int pos = _repoVehiculo.listarVehiculos().FindIndex(x => x.Id == p.VehiculoId);
+            if (pos >= 0)
+            {
+                _repoPoliza.agregarPoliza(p);
+            }
+            else
+            {
+                throw new Exception("No existe el vehiculo con Id = " + p.VehiculoId);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     } 
 }
